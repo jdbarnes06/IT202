25e041fe109fe56407ffccef6bb5e1a1a68d108f
Jonathan Barnes <jdbarnes06@gmail.com>
2022-03-02 19:17:19 -0500

    Password regex nearly done

--- LLAL.js
+++ LLAL.js
@@ -9,17 +9,41 @@ function validate()
         if (name.charAt(0) != name.charAt(0).toUpperCase())
         {
             name = name.charAt(0).toUpperCase() + name.substring(1);
-            names[i] = name;
         }
 
         if (name.substring(1) != name.substring(1).toLowerCase())
         {
-            names[i] = name.charAt(0) + name.substring(1).toLowerCase();
+            name = name.charAt(0) + name.substring(1).toLowerCase();
         }
 
         if (name.match(/^[A-Za-z]+$/) == null)
         {
             return alert("Names can only contain letters.");
         }
+
+        names[i] = name;
+    }
+
+    var password = document.getElementById("password").value;
+
+    if (password.length > 12)
+    {
+        return alert("Password can't have more than 12 characters.")
+    }
+
+    if (password.match(/[A-Z]+/) == null)
+    {
+        return alert("Password must have at least 1 uppercase letter.");
+    }
+
+    if (password.match(/[0-9]+/) == null)
+    {
+        return alert("Password must have at least 1 number.");
+    }
+
+    // All special characters ~`!@#$%^&*()_-+={[}]|\:;"'<,>.?/
+    if (password.match(/[]+/) == null)
+    {
+        return alert("Password must have at least 1 number.");
     }
 }
