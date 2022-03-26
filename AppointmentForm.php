62799502dc5db9693507d9679bd9e8e7001d1d65
Jonathan Barnes <jdbarnes06@gmail.com>
2022-03-26 17:24:37 -0400

    Finished Week 9 Coding Assignment

---
+++ AppointmentForm.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Form Output</title>
+</head>
+<body>
+    <?php
+        $name = $_POST["name"];
+        $service = $_POST["service"];
+        $number = $_POST["number"];
+        $price = $_POST["price"];
+        $tax = $_POST["tax"];
+        $total = $price * $number * (($tax / 100) + 1);
+
+        echo $name . ", you have " . $number . " appointment(s) for the
+             following procedure (" . $service . ") at the price of $"
+             . $price . " per visit with a tax rate of " . $tax . "%.
+             The total cost including tax is $" . $total . ".";
+    ?>
+</body>
+</html>
