70a067b78a14ff4d0bb66d4a2a1c60df399f6938
Jonathan Barnes <jdbarnes06@gmail.com>
2022-04-09 20:17:33 -0400

    Week 11 Assignment Done

---
+++ StudentForm.php
@@ -0,0 +1,41 @@
+﻿<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Student Form</title>
+</head>
+<body>
+    <form method="post">
+        <label for="id"><b>Student ID</b></label>
+        <input type="text" name="id"><br>
+        <input type="submit">
+    </form>
+
+    <?php
+        $servername = "sql1.njit.edu";
+        $username = "jb724";
+        $password = "ejmUc3O1%#W7";
+        $dbname = "jb724";
+
+        $con = mysqli_connect($servername,$username,$password,$dbname);
+
+        if (mysqli_connect_errno())
+        {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+
+        $id = $_POST['id'];
+        $sql = "SELECT ID FROM Student WHERE ID = $id";
+        $result = $con->query($sql);
+        
+        if ($result->num_rows > 0)
+        {
+            session_start();
+            $_SESSION['ID'] = $id;
+            echo "<script> location.href='StudentInfo.php'; </script>";
+        }
+        
+        $con->close();
+    ?>  
+</body>
+</html>
---
+++ StudentInfo.php
@@ -0,0 +1,54 @@
+﻿<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Student Info</title>
+</head>
+<body>
+    <?php
+        $servername = "sql1.njit.edu";
+        $username = "jb724";
+        $password = "ejmUc3O1%#W7";
+        $dbname = "jb724";
+
+        $con = mysqli_connect($servername,$username,$password,$dbname);
+
+        if (mysqli_connect_errno())
+        {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+
+        session_start();
+        $id = $_SESSION['ID'];
+        $sql = "SELECT Student.Name, Student.ID, Student.Major, Transcript.Course, Transcript.Grade
+        FROM Student
+        INNER JOIN Transcript ON Student.ID = Transcript.ID WHERE Student.ID = $id";
+        $result = $con->query($sql);
+
+        if ($result->num_rows > 0) 
+        {
+            echo "<table border='1px' cellpadding=5>";
+            echo "<tr><td>Name</td><td>ID</td><td>Major</td><td>Course</td><td>Grade</td></tr>";
+        
+            while ($row = $result->fetch_assoc()) 
+            {
+                echo "<tr>";
+                echo "<td>" . $row["Name"] . "</td>";
+                echo "<td>" . $row["ID"] . "</td>";
+                echo "<td>" . $row["Major"] . "</td>";
+                echo "<td>" . $row["Course"] . "</td>";
+                echo "<td>" . $row["Grade"] . "</td>";
+                echo "</tr>";
+            }
+
+            echo "</table>";
+        }
+
+        $con->close();
+    ?>
+
+    <a style="background-color: black; text-decoration: none; color: white; 
+              padding: 5px; position: relative; top: 10px;" 
+              href="https://web.njit.edu/~jb724/StudentForm.php">Home</a>
+</body>
+</html>
